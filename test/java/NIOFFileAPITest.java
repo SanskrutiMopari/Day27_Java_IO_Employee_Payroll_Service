7b4702d92a72c5a8147afce347a30ec9db04716f
Sanskruti Mopari <sanskrutimopari2016@gmail.com>
2023-01-09 17:28:32 +0530

    Added: Watch Directory all Files

--- test/java/NIOFFileAPITest.java
+++ test/java/NIOFFileAPITest.java
@@ -59,4 +59,11 @@ public class NIOFFileAPITest {
                 .forEach(System.out::println);
 
     }
+
+    @Test
+    public void givenDirectoryWhenWatchedListAllTheActivites() throws IOException {
+        Path dir = Paths.get(HOME + "/" + PLAY_WITH_NIO);
+        Files.list(dir).filter(Files::isRegularFile).forEach(System.out::println);
+        new Java8WatchService(dir).processEvents();
+    }
 }
